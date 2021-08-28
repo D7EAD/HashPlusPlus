500c8bec372fa2e4d6eef96371b238e1c7fd3975
Dread <42398281+D7EAD@users.noreply.github.com>
2021-08-27 22:38:49 -0400

    Update getHashFromBytes.cpp

--- examples/getHashFromBytes.cpp
+++ examples/getHashFromBytes.cpp
@@ -13,10 +13,11 @@ int main() {
 	// assign the SHA2-256 hash bytes of "sample text" to byteArray
 	uint8_t* byteArray = get::getBytes(ALGORITHMS::SHA2_256, "sample text");
 
-	// assign the hash object to _hex from getHash<uint8_t*>() for byte arrays
+	// assign the hex string of the 
 	hash _hex = get::getHash<uint8_t*>(ALGORITHMS::SHA2_256, byteArray);
 	// or use .getString()
 
-  	// print the hex representation of the string
 	std::cout << _hex;
+
+	delete[] byteArray;
 }
