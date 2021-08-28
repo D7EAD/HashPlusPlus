776057c74fd95aa8d05c1fd66082e92305bbe61a
Dread <42398281+D7EAD@users.noreply.github.com>
2021-08-27 22:40:18 -0400

    Update getHashFromBytes.cpp

--- examples/getHashFromBytes.cpp
+++ examples/getHashFromBytes.cpp
@@ -3,6 +3,8 @@
   getHashFromBytes.cpp
     Example to show how to get the hex representation of a hash from its bytes.
   
+    get::getHash<uint8_t*>() -> get hash from bytes
+    get::getHash() -> default -> get hash from raw data (string)
 */
 
 #include "hashpp.h"
@@ -13,7 +15,7 @@ int main() {
 	// assign the SHA2-256 hash bytes of "sample text" to byteArray
 	uint8_t* byteArray = get::getBytes(ALGORITHMS::SHA2_256, "sample text");
 
-	// assign the hex string of the 
+	// assign the hash object to _hex from getHash<uint8_t*>() for byte arrays
 	hash _hex = get::getHash<uint8_t*>(ALGORITHMS::SHA2_256, byteArray);
 	// or use .getString()
 
