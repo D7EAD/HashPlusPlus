ad89cebf81e12dc90b6320fcc6f98b86077cd644
Dread <42398281+D7EAD@users.noreply.github.com>
2021-08-27 23:04:57 -0400

    Update getHashFromBytes.cpp

--- examples/getHashFromBytes.cpp
+++ examples/getHashFromBytes.cpp
@@ -1,10 +1,10 @@
 /*
 
   getHashFromBytes.cpp
-    Example to show how to get the hex representation of a hash from its bytes.
-  
-    get::getHash<uint8_t*>() -> get hash from bytes
-    get::getHash() -> default -> get hash from raw data (string)
+	Example to show how to get the hex representation of a hash from its bytes.
+
+	get::getHash<uint8_t*>() -> get hash from bytes
+	get::getHash() -> default -> get hash from raw data (string)
 */
 
 #include "hashpp.h"
@@ -12,14 +12,12 @@
 using namespace hashpp;
 
 int main() {
-	// assign the SHA2-256 hash bytes of "sample text" to byteArray
-	uint8_t* byteArray = get::getBytes(ALGORITHMS::SHA2_256, "sample text");
+	// assign the MD5 hash bytes of "sample text" to byteArray
+	uint8_t byteArray[] = { 112, 238, 23, 56, 182, 178, 30, 44, 138, 67, 243, 165, 171, 14, 238, 113 };
 
 	// assign the hash object to _hex from getHash<uint8_t*>() for byte arrays
-	hash _hex = get::getHash<uint8_t*>(ALGORITHMS::SHA2_256, byteArray);
+	hash _hex = get::getHash<uint8_t*>(ALGORITHMS::MD5, byteArray);
 	// or use .getString()
 
 	std::cout << _hex;
-
-	delete[] byteArray;
 }
