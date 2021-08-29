db199c3272962fd170835a6532eb51e659a5df94
Dread <42398281+D7EAD@users.noreply.github.com>
2021-08-29 00:27:02 -0400

    Create getHashFromFile.cpp

---
+++ examples/getHashFromFile.cpp
@@ -0,0 +1,20 @@
+/*
+
+	getHashFromFile
+		Example to show how to get the hash of a single file.
+
+*/
+
+#include "hashpp.h"
+
+using namespace hashpp;
+
+int main() {
+	// get a hash of item1.txt in relative directory folder1
+	hash _a = get::getFileHash(ALGORITHMS::MD5, "./folder1/item1.txt");
+
+	std::cout << _a.getString() << std::endl;
+	// or _a.getString()
+
+	// output: 4817a091b69d5e89d7d5757e6ce19609 -> ./folder1/item1.txt
+}
