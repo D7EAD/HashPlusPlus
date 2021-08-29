903eefe4f66b2b550e8b38a696c4d2c29644b254
Dread <42398281+D7EAD@users.noreply.github.com>
2021-08-28 23:35:54 -0400

    Update getSimpleHashes.cpp

--- examples/getSimpleHashes.cpp
+++ examples/getSimpleHashes.cpp
@@ -13,21 +13,21 @@ int main() {
 	// get a hash collection containing the MD5 and MD4 hashes for all passed data
 	std::vector<std::string> data1 = { "data1", "data2", "data3", "data4", "data5", "data6", "data7", "data8", "data9" };
 	std::vector<std::string> data2 = { "data1", "data2", "data3", "data4", "data5", "data6", "data7", "data8", "data9" };
-	hashCollection collection2 = get::getHashes({ {ALGORITHMS::MD4, data1}, {ALGORITHMS::MD5, data2} });
+	hashCollection collection1 = get::getHashes({ {ALGORITHMS::MD4, data1}, {ALGORITHMS::MD5, data2} });
 
 	// the method above of getting multiple hashes is equivalent to the below
-	hashCollection collection = get::getHashes({ {ALGORITHMS::MD4, {"data1", "data2", "data3",
-		                                                        "data4", "data5", "data6",
-		                                                        "data7", "data8", "data9"} },
-						     {ALGORITHMS::MD5, {"data1", "data2", "data3",
-					                                "data4", "data5", "data6",
-					                                "data7", "data8", "data9"} } }); 
+	hashCollection collection2 = get::getHashes({ {ALGORITHMS::MD4, {"data1", "data2", "data3",
+		                                                         "data4", "data5", "data6",
+		                                                         "data7", "data8", "data9"} },
+						      {ALGORITHMS::MD5, {"data1", "data2", "data3",
+					                                 "data4", "data5", "data6",
+					                                 "data7", "data8", "data9"} } }); 
 	
 	// to iterate over hashes from a specific algorithm (in the order they were supplied)
-	for (auto i : collection["MD5"]) {
+	for (auto i : collection2["MD5"]) {
 		std::cout << i << std::endl;
 	}
-	for (auto i : collection["MD4"]) {
+	for (auto i : collection2["MD4"]) {
 		std::cout << i << std::endl;
 	}
 	
