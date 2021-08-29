23a213ccd74cfbe06b65ce4d2e35d0054ce6d286
Dread <42398281+D7EAD@users.noreply.github.com>
2021-08-29 00:27:25 -0400

    Update getHashFromFile.cpp

--- examples/getHashFromFile.cpp
+++ examples/getHashFromFile.cpp
@@ -13,7 +13,7 @@ int main() {
 	// get a hash of item1.txt in relative directory folder1
 	hash _a = get::getFileHash(ALGORITHMS::MD5, "./folder1/item1.txt");
 
-	std::cout << _a.getString() << std::endl;
+	std::cout << _a << std::endl;
 	// or _a.getString()
 
 	// output: 4817a091b69d5e89d7d5757e6ce19609 -> ./folder1/item1.txt
